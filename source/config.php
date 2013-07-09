be122494f0f4a38ba3c2100c73bac2a49480b8d0
rchampa <ricardocb48@gmail.com>
2013-07-09 04:46:21 +0200

    Config file server added.

---
+++ source/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "000000");
+define("DB_DATABASE", "gcm");
+ 
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyAJZoJNvWdrc3sLdfEi8Bw5vg-K1hp7xew"); // Place your Google API Key
+?>
